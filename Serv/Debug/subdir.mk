bf725aa467d2f932ef74ee823dff037a62341f09
AndresOtero <oteroandres95@gmail.com>
2015-10-21 15:14:00 -0300

    Linkeo proyectos

--- Serv/Debug/subdir.mk
+++ Serv/Debug/subdir.mk
@@ -35,7 +35,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -D__GXX_EXPERIMENTAL_CXX0X__ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -D__GXX_EXPERIMENTAL_CXX0X__ -I"/home/andres/git/AgeOfEmpiresTaller/AgeOfEmpiresTaller" -I"/home/andres/git/AgeOfEmpiresTaller/AgeOfEmpiresTaller/libs/plog-master/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
